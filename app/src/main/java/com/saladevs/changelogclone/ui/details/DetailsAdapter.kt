e9c7cda734f28483b177d367402f3f066d0d29b3
Gerard Sala <gsala24@gmail.com>
2017-08-01 22:00:39 +0200

    Implement grouping in details. Add databinding

--- app/src/main/java/com/saladevs/changelogclone/ui/details/DetailsAdapter.kt
+++ app/src/main/java/com/saladevs/changelogclone/ui/details/DetailsAdapter.kt
@@ -3,20 +3,15 @@ package com.saladevs.changelogclone.ui.details
 import android.support.v7.util.SortedList
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.util.SortedListAdapterCallback
-import android.text.TextUtils
-import android.text.format.DateUtils
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-
-import com.saladevs.changelogclone.App
-import com.saladevs.changelogclone.R
+import com.saladevs.changelogclone.databinding.RowDetailsCardDetailedBinding
+import com.saladevs.changelogclone.databinding.RowDetailsCardSimpleBinding
 import com.saladevs.changelogclone.model.PackageUpdate
 
-class DetailsAdapter : RecyclerView.Adapter<DetailsAdapter.ViewHolder>() {
+class DetailsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    private val mDataset = SortedList(PackageUpdate::class.java, object : SortedListAdapterCallback<PackageUpdate>(this) {
+    private val dataset = SortedList(PackageUpdate::class.java, object : SortedListAdapterCallback<PackageUpdate>(this) {
         override fun compare(o1: PackageUpdate, o2: PackageUpdate): Int {
             return o2.date.compareTo(o1.date)
         }
@@ -30,49 +25,88 @@ class DetailsAdapter : RecyclerView.Adapter<DetailsAdapter.ViewHolder>() {
         }
     })
 
+    private var viewTypes: List<Int> = emptyList()
 
     fun setData(updates: List<PackageUpdate>) {
-        mDataset.beginBatchedUpdates()
-        mDataset.addAll(updates)
-        mDataset.endBatchedUpdates()
+        dataset.beginBatchedUpdates()
+        dataset.addAll(updates)
+        viewTypes = extractViewTypes(dataset)
+        dataset.endBatchedUpdates()
+    }
+
+    private fun extractViewTypes(updates: SortedList<PackageUpdate>): List<Int> {
+        return MutableList(updates.size()) { updates.get(it) }
+                .foldRight("-1".to(mutableListOf<Int>()), { update, acc ->
+                    if (update.description == "No description available") {
+                        acc.apply { second.add(TYPE_ERROR) }
+                    } else if (update.description == acc.first) {
+                        acc.apply { second.add(TYPE_SIMPLE) }
+                    } else {
+                        Pair(update.description, acc.second.apply { add(TYPE_DETAILED) })
+                    }
+                }).second.reversed()
     }
 
     // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailsAdapter.ViewHolder {
-        // create a new view
-        val v = LayoutInflater.from(parent.context)
-                .inflate(R.layout.row_details_card, parent, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            TYPE_SIMPLE, TYPE_ERROR -> SimpleViewHolder(RowDetailsCardSimpleBinding.inflate(layoutInflater, parent, false))
+            TYPE_DETAILED -> DetailedViewHolder(RowDetailsCardDetailedBinding.inflate(layoutInflater, parent, false))
+            else -> throw IllegalArgumentException("Wrong view type")
+        }
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         // Get elements from dataset
-        val update = mDataset.get(position)
-
-        // Replace contents of the view
-        holder.subtitle.text = update.version
-
-        holder.title.text = DateUtils.formatDateTime(App.getContext(), update.date.time, 0)
-        if (TextUtils.isEmpty(update.description)) {
-            holder.support.visibility = View.GONE
-        } else {
-            holder.support.visibility = View.VISIBLE
-            holder.support.text = update.description
+        val update = dataset.get(position)
+        val isTop = position == 0
+        val isBot = position == dataset.size() - 1
+        val needsExtraSpacing = !isBot && getItemViewType(position + 1) == TYPE_DETAILED
+        when (holder) {
+            is SimpleViewHolder -> holder.bind(update, isTop, isBot, holder.itemViewType == TYPE_ERROR)
+            is DetailedViewHolder -> holder.bind(update, isTop, isBot, needsExtraSpacing)
         }
-
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount(): Int {
-        return mDataset.size()
+        return dataset.size()
     }
 
-    // Provide a reference to the views for each data item
-    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        var title: TextView = v.findViewById(R.id.title) as TextView
-        var subtitle: TextView = v.findViewById(R.id.subtitle) as TextView
-        var support: TextView = v.findViewById(R.id.support) as TextView
+    override fun getItemViewType(position: Int): Int {
+        return viewTypes[position]
+    }
+
+    class SimpleViewHolder(val binding: RowDetailsCardSimpleBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(update: PackageUpdate, top: Boolean, bottom: Boolean, error: Boolean) {
+            binding.update = update
+            binding.top = top
+            binding.bottom = bottom
+            binding.error = error
+            binding.executePendingBindings()
+        }
+    }
+
+    class DetailedViewHolder(val binding: RowDetailsCardDetailedBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(update: PackageUpdate, top: Boolean, bottom: Boolean, extraSpacing: Boolean) {
+            binding.update = update
+            binding.top = top
+            binding.bottom = bottom
+            binding.extraSpacing = extraSpacing
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        const val TYPE_DETAILED = 0
+        const val TYPE_SIMPLE = 1
+        const val TYPE_ERROR = 2
     }
 
 }
---
+++ app/src/main/java/com/saladevs/changelogclone/utils/Functions.kt
@@ -0,0 +1,30 @@
+package com.saladevs.changelogclone.utils
+
+import com.saladevs.changelogclone.R
+
+fun getDetailsSimpleLinkDrawable(top: Boolean, bottom: Boolean, error: Boolean): Int {
+    if (error) {
+        return when {
+            top && bottom -> R.drawable.ic_details_link_single_error
+            top -> R.drawable.ic_details_link_top_error
+            bottom -> R.drawable.ic_details_link_bot_error
+            else -> R.drawable.ic_details_link_mid_error
+        }
+    } else {
+        return when {
+            top && bottom -> R.drawable.ic_details_link_single_small
+            top -> R.drawable.ic_details_link_top_small
+            bottom -> R.drawable.ic_details_link_bot_small
+            else -> R.drawable.ic_details_link_mid_small
+        }
+    }
+}
+
+fun getDetailsDetailedLinkDrawable(top: Boolean, bottom: Boolean): Int {
+    return when {
+        top && bottom -> R.drawable.ic_details_link_single_big
+        top -> R.drawable.ic_details_link_top_big
+        bottom -> R.drawable.ic_details_link_bot_big
+        else -> R.drawable.ic_details_link_mid_big
+    }
+}
